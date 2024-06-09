7c155f47cd9a349725d079a3c883ab2dd19307ed
sriramadari <lakshmisriramadari1427@gmail.com>
2024-06-09 11:07:05 +0530

    login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,7 +33,8 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
       <Stack.Screen name="(auth)/login" 
-      options={{ headerShown: false }} />
+       options={{ headerShown: false }}
+          />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
       </Stack>
